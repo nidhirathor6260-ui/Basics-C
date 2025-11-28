3e3e24d44845006adac422b51e4d364be1db8157
nidhirathor6260-ui <nidhirathor6260@gmail.com>
2025-11-28 15:36:44 -0800

    possible or not

---
+++ triangle.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if((a+b) > c && (b+c)>a && (a+c) > b) cout << "Possible \n";
+    else cout << "Not possible";
+}
