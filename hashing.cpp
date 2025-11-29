9fc6a137facdb3b8bebaf43f1c915d22e8ee7829
nidhirathor6260-ui <nidhirathor6260@gmail.com>
2025-11-28 20:24:07 -0800

    hashing

---
+++ hashing.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int n ; 
+    cin >> n ;
+    int  arr[n];
+    for(int i = 0; i<n ; i++){
+        cin >> arr[i];
+    }
+    int hash[13]={0};
+    for(int i = 0 ; i<n ; i++){
+        hash[arr[i]]+=1;
+    }
+    int num;
+    cin >> num;
+    cout << hash[num]<< "\n";
+}
+// This code counts the occurrences of numbers in an array and retrieves the count for a specific number.
