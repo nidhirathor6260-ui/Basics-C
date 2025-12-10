8a2cb2ce616bdf54643297a544d67f38649111e5
nidhirathor6260-ui <nidhirathor6260@gmail.com>
2025-12-09 22:05:38 -0800

    Patterns

--- pattern.cpp
+++ pattern.cpp
@@ -25,4 +25,18 @@ int main(){
     cout << "\n";
   }
   
+    for(int i = 1 ; i<=n  ; i++){
+        for(int j = 1 ; j<=(n-i) ; j++){
+            cout << "  ";
+        }
+        for(int j = 1 ; j<i ; j++){
+            cout << j << " ";
+        }
+        for(int j = i ; j>=1 ; j--){
+            cout << j << " " ;
+        }
+        cout << "\n";
+    }
+    
+
 }
